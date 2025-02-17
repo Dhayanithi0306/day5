47abc37437ed19a85f7f597a1511e41c16507ff9
Dhayanithi0306 <Dhayanithi0306>
2025-02-17 09:55:25 +0530

    .

--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -18,7 +18,7 @@ function Login(){
     }
 
     function IsCheckedSetter(event){
-        // console.log(event.target.checked);
+        
         SetIsChecked(event.target.checked)
 
     }
@@ -51,15 +51,14 @@ return(<>
     <input type="checkbox" className="form-check-input" checked={IsChecked} onChange={IsCheckedSetter} required/>
     <label className="form-check-label">Confirm Login</label>
   </div>
-    {(Email.length>0 && Email!="hi123@gmail.com")?<p>Incorrect Email Address</p>:
-    (Password.length>0 && Password!="12345")?<p>Incorrect Password</p>:
-    (Email.length>0 && Password.length>0 && IsLoggedIn==true && IsChecked==false)?<p>please Confirm Login</p>:""}
+    {(Email.length>0 && Email!="user")?<p>Incorrect</p>:
+    (Password.length>0 && Password!="pass")?<p>Incorrect </p>:
+    (Email.length>0 && Password.length>0 && IsLoggedIn==true && IsChecked==false)?<p> Confirm Login</p>:""}
   <button type="submit" className="btn btn-danger" onClick={()=>{Submitted();SetLoggedIn(true)}} >Login</button>
 </form>
 </div>
 <div className="password">
-  <p><b>Email:</b> hi123@gmail.com</p>
-  <p><b>Password:</b> 12345</p>
+
 </div>
 </>)}
 export default Login;
