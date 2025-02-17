8619e1fb0c1f5cc23b59cbe2b9c72ccb6148c557
Dhayanithi0306 <Dhayanithi0306>
2025-02-17 10:26:32 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,79 @@
-const mysql=require("mysql2");
-require("dotenv").config();
+const bodyparser  = require('body-parser');
+const cors = require('cors');
 
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections:true, 
-    connectionLimit:10,
-    queueLimit:20,
-});
+const db = require("./src/config/db.js");
 
-connection.getConnection((err,conn)=>{
-    if(err){ 
-        console.error("Error connection to database: ");
-    }
-    else{
-        console.log("Connected to mysql database");
-        conn.release();
-    }
+const express = require('express');
+const app = express();
+app.use(bodyparser.json());
+app.use(cors());
+require('dotenv').config();
+ 
+const port = process.env.port || 8000;
+
+app.listen(port,()=>{
+    console.log("localhost:"+port);
 })
 
-module.exports = connection;
+app.get('/studentmarks',(req,res)=>{
+    const sql = 'select * from studentmarks';
+
+    db.query(sql,(err,result)=>{
+        if(err){
+            console.log(err);
+            return res.status(500).json({error:"failes to fetch marks"});
+        }
+        return res.status(200).json({marks:result});
+        
+    })
+})
+
+
+app.delete('/update-marks/:name',(req,res)=>{
+    const { name } = req.params;
+    const sql = 'DELETE FROM studentmarks WHERE name = ?';
+
+    db.query(sql,[name], (err,result)=>{
+        if(err){
+            console.log(err);
+            return res.status(500).json({error:"failes to delete marks"});
+        }
+        return res.status(200).json({marks:result});
+        
+    })
+})
+
+app.put('/update-marks/:name',(req,res)=>{
+    const { name } = req.params;
+    const {Name,s1,s2,s3,s4,s5} = req.body
+
+    const sql = `UPDATE studentmarks SET name=?,english=?,tamil=?,maths=?,physics=?,chemistry=? where name=?`
+db.query(sql,[Name,s1,s2,s3,s4,s5, name],(err)=>{
+    if(err){
+        console.log(err);
+        console.log("id",name)
+        console.log("put request failed:", Name,s1,s2,s3,s4,s5);
+        return res.status(500).json({err:"failed to add data"});
+    }
+    else{
+        console.log("id",name)
+        console.log("put request successful", Name,s1,s2,s3,s4,s5);
+    }
+    return res.status(200).json({message:"marks added successfuly"});
+    
+})
+})
+
+app.post('/update-marks',(req,res)=>{
+    const {Name,s1,s2,s3,s4,s5} = req.body
+
+    const sql = `insert into studentmarks (name,english,tamil,maths,physics,chemistry)
+values(?,?,?,?,?,?)`
+db.query(sql,[Name,s1,s2,s3,s4,s5],(err)=>{
+    if(err){
+        console.log(err);
+        return res.status(500).json({err:"failed to add data"});
+    }
+    return res.status(200).json({message:"marks added successfuly"});
+})
+})
